90ccbb9f7e4428692dec506e1afd998a61892abe
Numbers00 <kmaquino@up.edu.ph>
2022-08-20 18:56:16 +0800

    added styling and animation upon clicking wave-button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import * as React from "react";
-import { ethers } from "ethers";
+import React, {
+  useState,
+  useEffect,
+  useRef
+} from 'react';
+import { ethers } from 'ethers';
 import './App.css';
 
 export default function App() {
+  const defaultContainer = useRef(null);
+  const waveBtn = useRef(null);
 
   const wave = () => {
-    
+    defaultContainer.current.classList.add('waved');
+    waveBtn.current.classList.add('waved');
+    waveBtn.current.disabled = true;
   }
   
   return (
-    <div className="mainContainer">
-
-      <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
+    <div className='main-container'>
+      <div className='upper-container'>
+        <div className='default-container' ref={defaultContainer}>
+          <div className='default-container-header'>
+          👋 Hey there!
+          </div>
+          <div className='default-container-bio'>
+            I am Numbers00 and I especially like all things web-related. I don't really know what to put here, but connect
+            your Ethereum wallet and try waving at me!
+          </div>
+          <button className='wave-button' ref={waveBtn} onClick={wave}>
+            Wave?
+          </button>
         </div>
-
-        <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
-        </div>
-
-        <button className="waveButton" onClick={wave}>
-          Wave at Me
-        </button>
       </div>
     </div>
   );
